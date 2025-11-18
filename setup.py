e7a4baa1571ee7653e776e0746a282619bafaa06
Kevin Karbstein <40165174+KK260@users.noreply.github.com>
2025-11-18 15:19:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="ncbi-genome-tools",
+    version="0.1.0",
+    packages=["felix"],
+    entry_points={
+        "console_scripts": [
+            "findGenome = felix.findGenome:main",
+            "findClosestGenome = felix.findClosestGenome:main",
+            "assembleGenome = felix.assembleGenome:main",
+            "assembleGenes = felix.assembleGenes:main",
+        ]
+    },
+)
 

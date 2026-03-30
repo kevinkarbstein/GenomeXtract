c11fa6b51374861dcebeac99d9ca5b5a180354b4
Kevin Karbstein <k.karbstein@ufl.edu>
2026-03-30 09:00:26 +0200

    Update findClosestGenome.py

--- genomextract/findClosestGenome.py
+++ genomextract/findClosestGenome.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-This script searches the public NCBI database for the closest available plastome, mitogenome or nuclear genome sequence(s) from a given species or taxonomic rank. 
+This script searches the public NCBI database for the closest available plastiid, mitochondrial or nuclear genome sequence(s) from a given species or taxonomic rank. 
 
 License:
     Copyright 2026 Kevin Karbstein
